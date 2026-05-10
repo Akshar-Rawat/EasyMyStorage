afa936d2becce3c0eaf5ef4ba6ddf170b8cb92da
Akshar-Rawat <aksharrawat7@gmail.com>
2026-05-10 19:12:52 +0530

    fix: railway connection

--- backend/config.php
+++ backend/config.php
@@ -1,22 +1,15 @@
 <?php
+$host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
+$dbname = getenv('MYSQLDATABASE') ?: 'railway';
+$username = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: 'CshWgNmnrSMOHbIUODFswaDqCvqvEkQO';
+$port = getenv('MYSQLPORT') ?: '3306';
 
-
-$host = 'sql113.infinityfree.com';
-$dbname = 'if0_41880099_user_management';
-$username = 'if0_41880099';
-$password = '2o9qI2O9HhbZX2w';
 try {
-  
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-  
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
 } catch(PDOException $e) {
-
     die("Connection failed: " . $e->getMessage());
 }
 
--- backend/db.sql
+++ backend/db.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE IF NOT EXISTS user_management;
-USE user_management;
+USE railway;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
